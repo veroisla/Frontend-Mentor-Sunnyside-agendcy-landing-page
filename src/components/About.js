3862f9f6e134598315875829d684e78c200bc826
Veronica isla <veroislagleaniz@gmail.com>
2022-08-06 16:50:26 +0200

    finish layout article 1 and 2

--- src/components/About.js
+++ src/components/About.js
@@ -7,25 +7,31 @@ import '../styles/components/About.scss';
 function About() {
   return (
     <section className="about">
-      <article>
+      <article className="about__ArticleWhite">
         <div className="about__IntroImg about__ImgTransform"></div>
-        <h2> Transform your brand</h2>
-        <p>
-          We are a full-service creative agency specializing in helping brands
-          grow fast. Engage your clients through compelling visuals that do most
-          of the marketing for you.
-        </p>
-        <p>Learn more </p>
+        <div className="about__Content">
+          <h2 className="about__Title">Transform your brand</h2>
+          <p className="about__About">
+            We are a full-service creative agency specializing in helping brands
+            grow fast. Engage your clients through compelling visuals that do
+            most of the marketing for you.
+          </p>
+          <p className="about__LearnMore about__UnderlineYellow">learn more</p>
+        </div>
       </article>
-      <article>
+      <article className="about__ArticleWhite">
         <div className="about__IntroImg about__ImgStandOut"></div>
-        <h2> Stand out to the right audience</h2>
-        <p>
-          Using a collaborative formula of designers, researchers,
-          photographers, videographers, and copywriters, we’ll build and extend
-          your brand in digital places.
-        </p>
-        <p> Learn more</p>
+        <div className="about__Content">
+          <h2 className="about__Title"> Stand out to the right audience</h2>
+          <p className="about__About">
+            Using a collaborative formula of designers, researchers,
+            photographers, videographers, and copywriters, we’ll build and
+            extend your brand in digital places.
+          </p>
+          <p className="about__LearnMore about__UnderlinePink">learn more</p>
+        </div>
+
+        {/* ----------OTHER TYPE OF ARTICLE----------- */}
       </article>
       <article className="about__ImgBackground about__ImgGraphicDesign">
         <h3> Graphic design</h3>
@@ -35,10 +41,8 @@ function About() {
         </p>
       </article>
       <article className="about__ImgBackground about__ImgPhotography">
-        {' '}
         <h3>Photography</h3>
         <p>
-          {' '}
           Increase your credibility by getting the most stunning, high-quality
           photos that improve your business image.
         </p>
