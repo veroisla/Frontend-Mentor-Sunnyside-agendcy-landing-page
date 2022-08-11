77515c2f0f507ce4b6fb2bc05610079789c40057
Veronica isla <veroislagleaniz@gmail.com>
2022-08-11 12:52:11 +0200

    change layout dektop version in footer, mix-images and menu header

--- src/components/MixImages.js
+++ src/components/MixImages.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import '../styles/components/MixImages.scss';
+
+//MOBILE
 import Milk from '../images/mobile/image-gallery-milkbottles.jpg';
 import Orange from '../images/mobile/image-gallery-orange.jpg';
 import Cone from '../images/mobile/image-gallery-cone.jpg';
 import Sugar from '../images/mobile/image-gallery-sugar-cubes.jpg';
+
+//DESKTOP
+
+import MilkDesktop from '../images/desktop/image-gallery-milkbottles.jpg';
+import OrangeDesktop from '../images/desktop/image-gallery-orange.jpg';
+import ConeDesktop from '../images/desktop/image-gallery-cone.jpg';
+import SugarDesktop from '../images/desktop/image-gallery-sugarcubes.jpg';
 
 function MixImages() {
   return (
@@ -13,6 +22,29 @@ function MixImages() {
         <img src={Orange} alt="Orange" className="mixImages__Img" />
         <img src={Cone} alt="Icecream Cone" className="mixImages__Img" />
         <img src={Sugar} alt="Sugar Cubes" className="mixImages__Img" />
+      </section>
+
+      <section className="mixImagesDesktop">
+        <img
+          src={MilkDesktop}
+          alt="Milk bottles"
+          className="mixImagesDesktop__Img"
+        />
+        <img
+          src={OrangeDesktop}
+          alt="Orange"
+          className="mixImagesDesktop__Img"
+        />
+        <img
+          src={ConeDesktop}
+          alt="Icecream Cone"
+          className="mixImagesDesktop__Img"
+        />
+        <img
+          src={SugarDesktop}
+          alt="Sugar Cubes"
+          className="mixImagesDesktop__Img"
+        />
       </section>
     </>
   );
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -9,41 +9,48 @@ function Testimonials() {
     <section className="testimonials">
       {' '}
       <h4 className="testimonials__Title">client testimonials</h4>
-      <article className="testimonials__Article">
-        <img
-          src={Emily}
-          alt="Marketing Director"
-          className="testimonials__Img"
-        />
-        <p className="testimonials__About">
-          We put our trust in Sunnyside and they delivered, making sure our
-          needs were met and deadlines were always hit.{' '}
-        </p>
-        <p className="testimonials__Name"> Emily R.</p>
-        <p className="testimonials__Job"> Marketing Director</p>
-      </article>
-      <article className="testimonials__Article">
-        <img
-          src={Thomas}
-          alt="Chief Operating Officer"
-          className="testimonials__Img"
-        />
-        <p className="testimonials__About">
-          Sunnyside’s enthusiasm coupled with their keen interest in our brand’s
-          success made it a satisfying and enjoyable experience.{' '}
-        </p>
-        <p className="testimonials__Name">Thomas S. </p>
-        <p className="testimonials__Job">Chief Operating Officer</p>
-      </article>
-      <article className="testimonials__Article">
-        <img src={Jennie} alt="Business Owner" className="testimonials__Img" />
-        <p className="testimonials__About">
-          Incredible end result! Our sales increased over 400% when we worked
-          with Sunnyside. Highly recommended!
-        </p>
-        <p className="testimonials__Name"> Jennie F. </p>
-        <p className="testimonials__Job"> Business Owner </p>
-      </article>
+      <div className="testimonials__desktop">
+        {' '}
+        <article className="testimonials__Article">
+          <img
+            src={Emily}
+            alt="Marketing Director"
+            className="testimonials__Img"
+          />
+          <p className="testimonials__About">
+            We put our trust in Sunnyside and they delivered, making sure our
+            needs were met and deadlines were always hit.{' '}
+          </p>
+          <p className="testimonials__Name"> Emily R.</p>
+          <p className="testimonials__Job"> Marketing Director</p>
+        </article>
+        <article className="testimonials__Article">
+          <img
+            src={Thomas}
+            alt="Chief Operating Officer"
+            className="testimonials__Img"
+          />
+          <p className="testimonials__About">
+            Sunnyside’s enthusiasm coupled with their keen interest in our
+            brand’s success made it a satisfying and enjoyable experience.{' '}
+          </p>
+          <p className="testimonials__Name">Thomas S. </p>
+          <p className="testimonials__Job">Chief Operating Officer</p>
+        </article>
+        <article className="testimonials__Article">
+          <img
+            src={Jennie}
+            alt="Business Owner"
+            className="testimonials__Img"
+          />
+          <p className="testimonials__About">
+            Incredible end result! Our sales increased over 400% when we worked
+            with Sunnyside. Highly recommended!
+          </p>
+          <p className="testimonials__Name"> Jennie F. </p>
+          <p className="testimonials__Job"> Business Owner </p>
+        </article>
+      </div>
     </section>
   );
 }
